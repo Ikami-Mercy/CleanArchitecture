4a8bf3bf230b88e9b8779309bb6ce708d405b30d
Vincent Tirgei <tirgeic@gmail.com>
2018-09-30 11:00:01 +0300

    added posts pojo

---
+++ app/src/main/java/com/gelostech/cleanarchitecture/data/models/Post.kt
@@ -0,0 +1,18 @@
+package com.gelostech.cleanarchitecture.data.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Post(
+        @SerializedName("userId")
+        var userId: String? = null,
+
+        @SerializedName("id")
+        var id: Int? = null,
+
+        @SerializedName("title")
+        var title: String? = null,
+
+        @SerializedName("body")
+        var body: String? = null
+) {
+}
