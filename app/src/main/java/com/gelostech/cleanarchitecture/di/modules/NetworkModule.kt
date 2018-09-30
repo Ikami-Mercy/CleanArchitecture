ba29423a4814835a33d8e184f5700eed1bfac516
Vincent Tirgei <tirgeic@gmail.com>
2018-09-30 10:55:51 +0300

    added network module

---
+++ app/src/main/java/com/gelostech/cleanarchitecture/di/modules/NetworkModule.kt
@@ -0,0 +1,50 @@
+package com.gelostech.cleanarchitecture.di.modules
+
+import com.gelostech.cleanarchitecture.utils.ApiService
+import com.gelostech.cleanarchitecture.utils.Endpoints
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+open class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        val builder = GsonBuilder()
+        return builder.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttp(): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        return httpClient.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit {
+
+        return Retrofit.Builder()
+                .baseUrl(Endpoints.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun getApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
