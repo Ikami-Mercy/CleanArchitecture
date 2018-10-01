bca76cb7f0569ff0ddfe6be04aecaac505127a9f
Vincent Tirgei <tirgeic@gmail.com>
2018-10-01 13:23:36 +0300

    added app component initialization

--- app/src/main/java/com/gelostech/cleanarchitecture/MyApplication.kt
+++ app/src/main/java/com/gelostech/cleanarchitecture/MyApplication.kt
@@ -1,14 +1,36 @@
 package com.gelostech.cleanarchitecture
 
+import android.content.Context
 import android.support.multidex.MultiDexApplication
+import com.gelostech.cleanarchitecture.di.AppComponent
+import com.gelostech.cleanarchitecture.di.DaggerAppComponent
+import com.gelostech.cleanarchitecture.di.modules.AppModule
+import com.gelostech.cleanarchitecture.di.modules.NetworkModule
+import dagger.Component
 import timber.log.Timber
 
 class MyApplication : MultiDexApplication() {
+    private lateinit var appComponent: AppComponent
+    private lateinit var context: Context
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
+
+        context = this
+        appComponent = DaggerAppComponent.builder()
+                .appModule(AppModule(this))
+                .networkModule(NetworkModule())
+                .build()
+    }
+
+    fun getAppComponent(): AppComponent {
+        return appComponent
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
     }
 
 }
