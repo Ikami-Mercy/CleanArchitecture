3c6a9008cd7249ece1b11cefed409a3d8db9af10
Vincent Tirgei <tirgeic@gmail.com>
2018-10-01 13:47:52 +0300

    added ui to display items

--- app/src/main/java/com/gelostech/cleanarchitecture/ui/activities/MainActivity.kt
+++ app/src/main/java/com/gelostech/cleanarchitecture/ui/activities/MainActivity.kt
@@ -1,13 +1,70 @@
 package com.gelostech.cleanarchitecture.ui.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.gelostech.cleanarchitecture.MyApplication
 import com.gelostech.cleanarchitecture.R
+import com.gelostech.cleanarchitecture.data.Status
+import com.gelostech.cleanarchitecture.data.response.PostsResponse
+import com.gelostech.cleanarchitecture.di.ViewModelFactory
+import com.gelostech.cleanarchitecture.ui.adapters.PostsAdapter
+import com.gelostech.cleanarchitecture.ui.viewmodels.PostsViewModel
+import com.gelostech.cleanarchitecture.utils.hideView
+import com.gelostech.cleanarchitecture.utils.showView
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewmodel: PostsViewModel
+    private lateinit var adapter: PostsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        (application as MyApplication).getAppComponent().inject(this)
+
+        viewmodel = ViewModelProviders.of(this, viewModelFactory).get(PostsViewModel::class.java)
+
+        initViews()
+
+        viewmodel.posts().observe(this, Observer<PostsResponse> {
+            handleResponse(it!!)
+        })
+        viewmodel.fetchPosts()
+    }
+
+    private fun initViews() {
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+
+        adapter = PostsAdapter()
+        rv.adapter = adapter
+    }
+
+    private fun handleResponse(response: PostsResponse) {
+        when(response.status) {
+            Status.LOADING -> {
+                Timber.e("Loading posts...")
+                loading.showView()
+            }
+
+            Status.SUCCESS -> {
+                Timber.e("Fetched posts")
+                loading.hideView()
+                adapter.addPosts(response.posts!!)
+            }
+
+            Status.ERROR -> {
+                Timber.e("Error fetching posts")
+                loading.hideView()
+                error.showView()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/gelostech/cleanarchitecture/ui/adapters/PostsAdapter.kt
@@ -0,0 +1,36 @@
+package com.gelostech.cleanarchitecture.ui.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.gelostech.cleanarchitecture.R
+import com.gelostech.cleanarchitecture.data.models.Post
+import com.gelostech.cleanarchitecture.databinding.ItemPostBinding
+import com.gelostech.cleanarchitecture.utils.inflate
+
+class PostsAdapter : RecyclerView.Adapter<PostsAdapter.PostHolder>() {
+    private val posts = mutableListOf<Post>()
+
+    fun addPosts(posts: List<Post>) {
+        this.posts.addAll(posts)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): PostHolder {
+        return PostHolder(p0.inflate(R.layout.item_post))
+    }
+
+    override fun getItemCount(): Int = posts.size
+
+    override fun onBindViewHolder(p0: PostHolder, p1: Int) {
+        p0.bind(posts[p1])
+    }
+
+    class PostHolder(private val binding: ItemPostBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(post: Post) {
+            binding.post = post
+        }
+
+    }
+
+}
